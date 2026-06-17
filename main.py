2cca906925d04161028dd8b01edbaa444679cc2c
lufionepic1994 <lufionepic1994@gmail.com>
2026-06-17 10:25:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -737,7 +737,7 @@ PANEL_HTML = r"""<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<title data-en="Luffy Panel" data-fa="پنل لافی">Luffy Panel</title>
+<title data-en="Luffy Panel" data-fa="LUFFY PANEL">Luffy Panel</title>
 <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700;900&family=Inter:wght@300;400;500;600;700&family=Vazirmatn:wght@400;600;700;800&display=swap" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js"></script>
 <style>
